51c9ab4fa1d6ba4f383875a887d96a35ebcac121
Massimiliano Giovagnoli <massimiliano.giovagnoli.1992@gmail.com>
2020-12-23 20:07:24 +0100

    feat(users-configuration.nix): configure subuid/subgid ranges

--- default.nix
+++ default.nix
@@ -8,6 +8,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      ./users-configuration.nix
       ./podman.nix
     ];
 
---
+++ users-configuration.nix
@@ -0,0 +1,10 @@
+{
+  users = {
+    users = {
+      massi = {
+        subUidRanges = [{ startUid = 100000; count = 65536; }];
+        subGidRanges = [{ startGid = 100000; count = 65536; }];
+      };
+    };
+  };
+}
