a929384a76911453b28ce3032c1219b8a3c28cd7
Massimiliano Giovagnoli <massimiliano.giovagnoli.1992@gmail.com>
2020-12-23 21:48:34 +0100

    feat(users): add massi to wheel group

--- users-configuration.nix
+++ users-configuration.nix
@@ -2,6 +2,8 @@
   users = {
     users = {
       massi = {
+        isNormalUser = true;
+        extraGroups = [ "wheel" ];
         subUidRanges = [{ startUid = 100000; count = 65536; }];
         subGidRanges = [{ startGid = 100000; count = 65536; }];
       };
