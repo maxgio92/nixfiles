a4de94485c53290de25deb93dd649e44fa8f7813
Massimiliano Giovagnoli <massimiliano.giovagnoli.1992@gmail.com>
2020-12-23 20:18:20 +0100

    style(podman.nix): follow *-configuration.nix convention

--- default.nix
+++ default.nix
@@ -9,7 +9,7 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
       ./users-configuration.nix
-      ./podman.nix
+      ./podman-configuration.nix
     ];
 
   # Use the GRUB 2 boot loader.
