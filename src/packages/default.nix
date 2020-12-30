1a1f8ed955ea879bc645ddf2b6de0728fcb612ff
Massimiliano Giovagnoli <massimiliano.giovagnoli.1992@gmail.com>
2020-12-30 16:31:27 +0100

    feat(src/packages): add spotify to system pkgs

--- src/packages/default.nix
+++ src/packages/default.nix
@@ -1,11 +1,15 @@
 { pkgs, ... }:
 {
+
+  nixpkgs.config.allowUnfree = true;
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     wget vim
     firefox
     tmux
+    spotify
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
