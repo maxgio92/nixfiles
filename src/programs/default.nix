55b54f2e5b9db897efa2f5fb7a55670e3d2fa08d
maxgio92 <massimiliano.giovagnoli.1992@gmail.com>
2021-07-03 11:23:22 +0200

    feat: add gpg-agent program

--- src/programs/default.nix
+++ src/programs/default.nix
@@ -2,8 +2,8 @@
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  programs.gnupg.agent = {
+    enable = true;
+    #enableSSHSupport = true;
+  };
 }
